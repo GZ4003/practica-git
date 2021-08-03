fe0f3b5e21d08569bbc478bac5ce3fadd68f2091
GZ4003 <you@example.com>
2021-08-03 17:03:25 -0300

    prueba-ahora

---
+++ index.js
@@ -0,0 +1,109 @@
+//var nombre = 'alexis';
+//console.log(nombre)
+//var nombre = 'ivan';
+//console.log(nombre)
+//var nombre = 'gonzalo';
+//console.log(nombre)
+//var nombre= 'camila';
+//console.log(nombre)
+//var nombre = 'maira';
+///console.log(nombre)
+
+//console.log(2+2 === 4 && 2+6 === 8)
+
+//console.log(4+4 !== 7 && 14+14 === 28)
+
+//console.log(10+10 === 20 && 10+30 === 40)
+
+//console.log(2+2 !== 80 && 15+62 !== 456)
+
+//console.log(20+20 === 120 || 2+4 === 6)
+
+//const conditionalIf = () => {
+    //if(10 + 10 === 20){
+        //console.log('Es cierto')
+    //}
+//}
+//console.log(conditionalIf())
+
+//const conditional = () => {
+    //if(10 +20 === 40){
+        //console.log('Es verdad')
+    //} else{
+        //console.log('valor invalido')
+    //}
+//}
+
+//console.log(conditional())
+
+//const condionalIfelseIf = () => {
+//    if (10+10 === 100){
+//        console.log('es cierto')
+//    } else if(10 + 10 === 1) {
+//        console.log('valor invalido')
+//    } else {
+//        console.log('no sabes nada')
+//    }
+//}
+
+//console.log(condionalIfelseIf())
+
+//const pruebaif = () => {
+//    if(2 +2  === 5){
+//        console.log('correcto')
+//    } else if ( 2 + 2 !== 4){
+//        console.log('tambien esta bien')
+//    } else {
+//        console.log('volver a hacer')
+//    }
+//}
+
+//console.log(pruebaif())
+
+//const ultimo = () => {
+  //  if(1+1 === 3){
+   //     console.log('es correcto pa')
+   // } else if (1 + 1 !== 6 ){
+   //     console.log('puede ser pa')
+  //  } else{
+  //      console.log('puede fallar')
+  //  }
+//} 
+
+//console.log(ultimo())
+
+//const final = () => {
+    //if(1+3 === 4){
+        //console.log('hiciste bien')
+    //} else if (1+5 !== 6){
+        //console.log('tambien es correcto')
+    //} else{
+      //  console.log('felicidades entediste el tema')
+  //  }
+//}
+
+//console.log(final())
+
+const ternario = 1 + 3 === 6 ? console.log('esta bien') : console.log('esta mal')
+
+console.log(ternario)
+
+const ternario2 = 1 + 4 !== 5 ? console.log('todo esta ok') : console.log('hacelo de vuelta')
+
+console.log(ternario2)
+
+const ternario3 = 11 + 11 === 22 ? console.log('bien') : console.log('muy bien')
+
+console.log(ternario3)
+
+const ternario4 = 23 + 22 === 45 ? console.log('bien che') : console.log('volver a tipiar')
+
+console.log(ternario3)
+
+const ternariofinal = 2 + 2 === 4 ? console.log('okey') : console .log('él pepe')
+
+console.log(ternariofinal)
+
+const ternario = 2 + 2 !== 4 ? console.log('muy bien') : console.log('sos crack')
+
+console.log(ternario)
